566efa083d829d66c647246da2f8ec2cb1f73153
Muhammad Uwais Baig <premierhub.com@gmail.com>
2026-06-14 18:43:40 +0500

    feat: add Assignment 15 Sign-In and Sign-Up system
    Uploaded the C# source code, project files, and visual documentation for Assignment 15[cite: 25]. This console application implements a secure Sign-In and Sign-Up authentication system[cite: 25]. It demonstrates structural best practices by decoupling user-related business logic into a dedicated BL/MUser.cs class file, keeping the main execution layer clean[cite: 25].

---
+++ Assignment_15_AuthenticationSystem/Assesment15 _SignIn_SignUp/BL/MUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment_15_SignUp_Classes.BL
+{
+    public class MUser
+    {
+        public string userName;
+        public string userPassword;
+        public string userRole;
+        public static List<MUser> usersList = new List<MUser>();
+        public MUser(string userName, string userPassword, string userRole)
+        {
+            this.userName = userName;
+            this.userPassword = userPassword;
+            this.userRole = userRole;
+        }
+
+        public bool isAdmin()
+        {
+            if (userRole == "Admin")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool addUserIntoList(MUser user)
+        {
+            foreach (MUser storedUser in usersList)
+            {
+                if (storedUser.userName.ToLower() == user.userName.ToLower())
+                {
+                    return false;
+                }
+            }
+            usersList.Add(user);
+            return true;
+        }
+        public static MUser isValid(MUser user)
+        {
+            foreach (MUser storedUser in usersList)
+            {
+                if (storedUser.userName == user.userName && storedUser.userPassword == user.userPassword)
+                {
+                    return storedUser;
+                }
+            }
+            return null;
+        }
+    }
+}
